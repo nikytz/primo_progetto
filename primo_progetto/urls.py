48c49ab1fe1877bf799a562af3d46609ead58050
nikytz <nicole.tuzzeo@gmail.com>
2025-11-10 13:28:04 +0100

    passo11
    fatto passo 11

--- primo_progetto/urls.py
+++ primo_progetto/urls.py
@@ -21,5 +21,6 @@ from .views import index_root
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('prima_app/',include("prima_app.urls",namespace="prima_app")),
+    path('seconda_app/',include("seconda_app.urls",namespace="seconda_app")),
     path('',index_root)
 ]
