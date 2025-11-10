1ef4cbddc27423360e976df01381fe01b3b90f22
nikytz <nicole.tuzzeo@gmail.com>
2025-11-10 12:15:48 +0100

    passo 8
    passa 8 rifetto

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,10 +3,10 @@ from prima_app.views import homepage, welcome, lista, chi_siamo, variabili, inde
 
 app_name="prima_app"
 urlpatterns=[
-    path('',homepage, name='homepage'),
+    path('homepage',homepage, name='homepage'),
     path('welcome',welcome, name='welcome'),
     path('lista',lista,name='lista'),
     path('chi_siamo',chi_siamo,name='chi_siamo'),
     path('variabili',variabili,name='variabili'),
-    path('index',index,name='index'),
+    path('',index,name='index'),
 ]
